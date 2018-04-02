2503aef1a27da5b7ad5b885d45f29a7000879a84
Lu Fan Zhong <lufanzhong@gmail.com>
2018-04-03 01:01:16 +0800

    Minor repairs

--- files/root/usr/share/shadowsocksr/update.sh
+++ files/root/usr/share/shadowsocksr/update.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-temp_dir="/etc/shadowsocksr/"
+conf_dir="/etc/shadowsocksr/"
 script_dir="/usr/share/shadowsocksr/"
-${script_dir}chnroute.sh ${temp_dir}china_chnroute.txt
-${script_dir}gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5300 -o /etc/shadowsocksr/dnsmasq_gfwlist.conf
+${script_dir}chnroute.sh ${conf_dir}china_chnroute.txt
+${script_dir}gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5300 -o ${conf_dir}dnsmasq_gfwlist.conf
 /etc/init.d/shadowsocksr restart
